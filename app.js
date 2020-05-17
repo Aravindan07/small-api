31211084b87be046f129ac3777d70fd6413a448f
Aravindan <aravindanravi33@gmail.com>
2020-05-17 23:49:59 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postRoutes = require("./routes/posts");
+
+//Middlewares
+app.use("/posts", postRoutes);
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("We are on the Home Page");
+});
+
+//Connect to MongoDB
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
+
+//Listening Port
+app.listen(8080);
